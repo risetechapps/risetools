c7f28e42f29763b58cf54d6969d037ac7eb8fce3
risetechapps <apps@risetech.com.br>
2025-11-11 00:32:45 -0300

    Criado e registrado Device

--- src/RiseToolsServiceProvider.php
+++ src/RiseToolsServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace RiseTechApps\RiseTools;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Routing\ResponseFactory;
 use Illuminate\Support\ServiceProvider;
+use RiseTechApps\RiseTools\Features\Device\Device;
 use Symfony\Component\HttpFoundation\Response;
 
 class RiseToolsServiceProvider extends ServiceProvider
@@ -21,13 +23,15 @@ class RiseToolsServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-
+        $this->app->singleton(Device::class, function ($app) {
+            return new Device();
+        });
     }
 
     protected function registerMacrosResponse(): void
     {
         if (!ResponseFactory::hasMacro('jsonBase')) {
-            ResponseFactory::macro('jsonBase', function (bool $success, string $message = null, array $data = null, int $code = Response::HTTP_OK) {
+            ResponseFactory::macro('jsonBase', function (bool $success, string $message = null, array|JsonResource $data = null, int $code = Response::HTTP_OK) {
                 $response = [
                     'success' => $success,
                     'code' => $code,
@@ -38,7 +42,12 @@ class RiseToolsServiceProvider extends ServiceProvider
                 }
 
                 if (!empty($data)) {
-                    $response['data'] = $data;
+
+                    if($data instanceof JsonResource) {
+                        $response['data'] = $data->jsonSerialize();
+                    }else{
+                        $response['data'] = $data;
+                    }
                 }
 
                 return response()->json($response, $code);
@@ -46,31 +55,31 @@ class RiseToolsServiceProvider extends ServiceProvider
         }
 
         if (!ResponseFactory::hasMacro('jsonSuccess')) {
-            ResponseFactory::macro('jsonSuccess', function ($data = [], string $message = 'Operation completed successfully.') {
+            ResponseFactory::macro('jsonSuccess', function (array|JsonResource $data = null, string $message = 'Operation completed successfully.') {
                 return response()->jsonBase(true, $message, $data, Response::HTTP_OK);
             });
         }
 
         if (!ResponseFactory::hasMacro('jsonError')) {
-            ResponseFactory::macro('jsonError', function (string $message = 'Resource not available.', array $data = null) {
+            ResponseFactory::macro('jsonError', function (string $message = 'Resource not available.', array|JsonResource $data = null) {
                 return response()->jsonBase(false, $message, $data, Response::HTTP_UNPROCESSABLE_ENTITY);
             });
         }
 
         if (!ResponseFactory::hasMacro('jsonGone')) {
-            ResponseFactory::macro('jsonGone', function (string $message = 'Recurso não disponível.', array $data = null) {
+            ResponseFactory::macro('jsonGone', function (string $message = 'Recurso não disponível.', array|JsonResource $data = null) {
                 return response()->jsonBase(false, $message, $data, Response::HTTP_GONE);
             });
         }
 
         if (!ResponseFactory::hasMacro('jsonNotFound')) {
-            ResponseFactory::macro('jsonNotFound', function (string $message = 'Resource not found.', array $data = null) {
+            ResponseFactory::macro('jsonNotFound', function (string $message = 'Resource not found.', array|JsonResource $data = null) {
                 return response()->jsonBase(false, $message, $data, Response::HTTP_NOT_FOUND);
             });
         }
 
         if (!ResponseFactory::hasMacro('jsonInternal')) {
-            ResponseFactory::macro('jsonInternal', function (string $message = 'Internal server error.', array $data = null) {
+            ResponseFactory::macro('jsonInternal', function (string $message = 'Internal server error.', array|JsonResource $data = null) {
                 return response()->jsonBase(false, $message, $data, Response::HTTP_INTERNAL_SERVER_ERROR);
             });
         }
